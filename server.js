c54aa845ec37a32e3e8aab05c53a9d048045a653
Mark Powalisz <markpowalisz@Fenix-Macbook-Pro.local>
2018-02-05 18:35:59 -0500

    Updated server.js. Deployed to heroku. Added css and js dependencies

--- server.js
+++ server.js
@@ -2,3 +2,23 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var logger = require("morgan");
 var mongoose = require("mongoose");
+
+var request = require("request");
+var cheerio = require("cheerio");
+
+// Require all models
+var db = require("./models");
+
+var PORT = process.env.PORT || 3000;
+
+// Initialize Express
+var app = express();
+
+// Configure middleware
+
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+// Use body-parser for handling form submissions
+app.use(bodyParser.urlencoded({ extended: false }));
+// Use express.static to serve the public folder as a static directory
+app.use(express.static("public"));
